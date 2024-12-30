14cb99230a168cd158847e973cb4ac15da45ae91
xxbeann <yjb4945@gmail.com>
2024-12-30 20:46:33 +0900

    HelloControler에 ResponseDTO 추가

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 // 패키지 선언: 이 클래스가 속한 패키지
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 이 클래스가 Spring MVC 컨트롤러임을 나타냄 반환된 값이 view 로 매핑되지 않고 클라이언트에 직접 반환
@@ -11,5 +13,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+    // 클라이언트로부터 name과 amount값을 요청 파라미터로 받아 DTO 객체 생성 후 JSON 포맷으로 변환
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") Integer amount){
+        // RequestParam 어노테이션 - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+        // name 과 amount는 API를 호출하는 곳에서 넘겨준 값들.
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // Junit4에서 테스트 실행기를 지정. SpringRunner를 사용하여 Spring Context를 로드하고, 필요한 빈을 주입하며 테스트를 실행
 // 스프링 부트 테스트와 Junit 사이에 연결자 역할을 수행
@@ -22,13 +22,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 // 클래스 선언
 public class HelloControllerTest {
-    
+
     // Spring이 MockMvc객체(Bean)를 자동으로 주입
     // MockMvc 컨트롤러의 요청과 응답을 테스트 할 수 있는 가짜 MVC 환경을 제공
     @Autowired
     // MockMvc 객체를 통해 HTTP 요청을 가상으로 보내고, 응답을 검증하는 데 사용
     private MockMvc mvc;
-    
+
     // 테스트 메서드
     // 이 메서드가 테스트 메서드임을 나타내는 어노테이션 - Junit에 의해 실행
     @Test
@@ -40,5 +40,17 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
