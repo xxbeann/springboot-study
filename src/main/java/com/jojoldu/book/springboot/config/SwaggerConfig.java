7897709069cc689b8aae2fd0ab251be8e2b2b7b4
xxbeann <yjb4945@gmail.com>
2025-01-01 13:19:56 +0900

    chore: swagger 설정

---
+++ src/main/java/com/jojoldu/book/springboot/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.jojoldu.book.springboot.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private final String TITLE = "title";
+    private final String DESCRIPTION = "description";
+    private final String VERSION = "V1.0.0";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                );
+    }
+
+}
